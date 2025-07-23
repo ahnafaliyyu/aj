a163c7b827e9a2c4e3992fea1c1d724edfb52b19
ahnafaliyyu <aahnaf608@gmail.com>
2025-07-23 19:44:55 +0800

    editing

--- js/rspn.js
+++ js/rspn.js
@@ -1,10 +1,14 @@
-const navbar= document.getElementsByTagName('nav')[0];
-window.addEventListener('scroll', function(){
-    console.log(window.scrollY);
-    if (window.scrollY>1){
-        navbar.classList.replace('bg-transparent','nav-color')
-    }else if(this.window.scrollY<=0){
-        navbar.classList.replace('nav-color','bg-transparent')
+const navbarDesktop = document.querySelector('.navbar-desktop');
+const navbarMobile = document.querySelector('.navbar-mobile');
 
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 1) {
+        // Tambah warna latar saat scroll
+        navbarDesktop?.classList.replace('bg-transparent', 'nav-color');
+        navbarMobile?.classList.replace('bg-transparent', 'nav-color');
+    } else {
+        // Kembalikan transparan
+        navbarDesktop?.classList.replace('nav-color', 'bg-transparent');
+        navbarMobile?.classList.replace('nav-color', 'bg-transparent');
     }
-})
+});
